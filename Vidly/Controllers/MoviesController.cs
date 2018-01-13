123d0921d9a8645a3b28617dec8addd0acc71201
Tommy Kovalchuk <34948289+tommypk17@users.noreply.github.com>
2018-01-13 12:11:47 -0500

    First .Net App

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer{ Name = "Customer1"},
+                new Customer{ Name = "Customer2"}
+
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
